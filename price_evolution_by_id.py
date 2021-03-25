b7c1ee4a27f9546fecb534a8a80c98a6482f88fe
Carlos-Acosta <45806570+Carlos-Acosta@users.noreply.github.com>
2021-03-25 13:08:12 +0800

    Cambios grafica evolución temporal

--- price_evolution_by_id.py
+++ price_evolution_by_id.py
@@ -6,9 +6,15 @@ import matplotlib.pyplot as plt
 def evolucion_precio_gasolinera(csv, identificador, producto):
     df = pd.read_csv(csv)
     df_id = df.loc[df['id'] == identificador]
-    sns.lineplot(x = "Fecha", y = producto, data=df_id)
-    plt.title("Gasolinera " + identificador + ": Evolución temporal precio")
-    plt.xticks(rotation=90)
-    plt.show()
 
-evolucion_precio_gasolinera("bonarea_gasolineras.csv", "G058", "GASOLINA S/P 95")
+    if df_id[producto].sum(axis = 0, skipna = True) == 0:
+        print("There is no data for this selection")
+    else:
+        sns.lineplot(x = "Fecha", y = producto, data=df_id)
+        plt.title("Gasolinera " + identificador + ": Evolución temporal precio")
+        plt.xticks(rotation=90)
+        plt.show()
+        
+evolucion_precio_gasolinera("bonarea_gasolineras.csv",
+                            "G027",
+                            "GASOLINA S/P 98")
