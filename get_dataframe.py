1b9abfaef6e612662aef4040861004f3553bdcbe
Carlos-Acosta <45806570+Carlos-Acosta@users.noreply.github.com>
2021-03-28 23:22:14 +0800

    Cambios get_dataframe

--- get_dataframe.py
+++ get_dataframe.py
@@ -15,7 +15,16 @@ from main_functions import get_data_by_id
 from main_functions import get_id_by_entityType
 from main_functions import get_df_row_by_id
 from main_functions import update_dataframe
+from main_functions import update_prices
+from main_functions import update_data_and_prices
+from main_functions import update_dataframe
 
 
-update_dataframe(get_id_by_entityType("benzinera"))
 
+#update_dataframe(get_id_by_entityType("benzinera"))
+
+update_prices("benzinera")
+
+update_data_and_prices("benzinera")
+
+update_dataframe("benzinera")
