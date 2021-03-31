a1e3fc2f963ced8fb5ae09c65237ca2ce9159ab0
ogarces <lettertoolia@gmail.com>
2021-03-31 16:38:57 +0200

    cambios en funciones - ver olgatest para probar

--- main_functions.py
+++ main_functions.py
@@ -46,16 +46,19 @@ def get_response(request_str):
     return resp
 
 
-def get_data_by_id(id):
+def get_response1(entity_type="benzinera"):
+    """[summary]
+        Uses a search string to place a post request. Returns the response formatted as json
+    Args:
+        request_str ([str]): [a search string that contains the parameters necessary to place a post request]
     """
-    gets a response for all relevant information belonging to an id. At this point the function just sends a request to the ajax page
-    and gets back a json code
-    """
+
+    data = build_request([entity_type])
     headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'origin': 'https://www.bonarea.com',
-            'Referer': 'https://www.bonarea.com/ca/default/locate'}
-    url = 'https://www.bonarea-agrupa.com/locator/Localitzador/GetByID'
-    data = "id="+id+"&tipus=BENZINERA&language=ca"
+               'origin': 'https://www.bonarea.com',
+               'Referer': 'https://www.bonarea.com/ca/default/locate'}
+
+    url = 'https://www.bonarea-agrupa.com/locator/Localitzador/Get'
     response = requests.post(url, data=data, headers=headers)
     resp = response.json()
     return resp
@@ -73,11 +76,56 @@ def get_id_by_entityType(entity_type="benzinera"):
 
     return ids
 
+def get_id_by_entityType1(entity_type="benzinera"):
+    """
+    uses the response from get_response and extracts data such as id, coordenades, type, etc. 
+    not all data types can be extracted from this view. So we return a list of ids and iterate over them later. 
+    """
+    response = get_response1(entity_type)
+    ids = []
+    for data in response:
+        id = data["id"]
+        ids.append(id)
 
-def get_df_row_by_id(gasolinera_id):
+    return (ids, entity_type)
+
+
+
+def get_data_by_id(id):
+    """
+    gets a response for all relevant information belonging to an id. At this point the function just sends a request to the ajax page
+    and gets back a json code
+    """
+    headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'origin': 'https://www.bonarea.com',
+            'Referer': 'https://www.bonarea.com/ca/default/locate'}
+    url = 'https://www.bonarea-agrupa.com/locator/Localitzador/GetByID'
+    data = "id="+id+"&tipus=BENZINERA&language=ca"
+    response = requests.post(url, data=data, headers=headers)
+    resp = response.json()
+    return resp
+
+def get_data_by_id1(id, entity_type="benzinera"):
+    """
+    gets a response for all relevant information belonging to an id. At this point the function just sends a request to the ajax page
+    and gets back a json code
+    """
+    
+    headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'origin': 'https://www.bonarea.com',
+            'Referer': 'https://www.bonarea.com/ca/default/locate'}
+    url = 'https://www.bonarea-agrupa.com/locator/Localitzador/GetByID'
+    data = "id="+str(id)+"&tipus="+entity_type+"&language=ca"
+    response = requests.post(url, data=data, headers=headers)
+    resp = response.json()
+    return resp
+
+
+
+def get_df_row_by_id(gasolinera_id, entity_type="benzinera"):
     """gets a non-nested dafaframe of one row with all related petrol station values"""
-
-    dicc = get_data_by_id(gasolinera_id)
+    #check here
+    dicc = get_data_by_id1(gasolinera_id, entity_type)
     df_dicc = pd.DataFrame([dicc])
 
     df_dicc["Fecha"] = datetime.today().strftime('%d-%m-%Y')
@@ -182,3 +230,20 @@ def update_data_and_prices(entity_type = "benzinera"):
                 csvfile.write(get_df_row_by_id(id_gasolineras_list[i])[2].to_csv(index=False, header=False))
     return
     
+def update_dataframe1(out_file, entity_type = "benzinera"):
+    """Creates dataframe if csv file does not exist and 
+    updates existing dataframes with new data
+    """
+
+    # eliminar el 1 del nombre del csv
+    id_gasolineras_list = get_id_by_entityType1(entity_type)
+    csv_name = out_file
+    if os.path.isfile(csv_name):
+        pass
+    else:
+        with open(csv_name,'a', newline='') as csvfile:
+            csvfile.write(get_df_row_by_id(id_gasolineras_list[0], entity_type)[0].to_csv(index=False, header=True))
+            for i in range(1,len(id_gasolineras_list)):
+                csvfile.write(get_df_row_by_id(id_gasolineras_list[i], entity_type)[0].to_csv(index=False, header=False))
+        
+    return
---
+++ olgatest.py
@@ -0,0 +1,32 @@
+from main_functions import get_response1, get_data_by_id1, update_dataframe1, build_request, get_id_by_entityType1, get_df_row_by_id
+
+
+# datos por id, en función del id y tipo de intidad
+print(get_data_by_id1("42376", "BOTIGA"))
+
+
+# actualiza dataframe con el nmbre del csv y el tipo de entidad - comprueba bot, box, buf csv
+update_dataframe1("ben.csv", "benzinera")
+update_dataframe1("box.csv", "box")
+update_dataframe1("buf.csv", "bufet")
+
+
+
+
+# tu función con poco cambio, le metes id, devuelve línea
+# NOTA: hay que pasarle el tipo de entidad, sino por defecto es benzinera
+
+print("\n\n\n formato incorrecto \n\n\n")
+print(get_df_row_by_id(42305)) # no funciona
+
+print("\n\n\n formato correcto \n\n\n")
+print(get_df_row_by_id("42305", "botiga")) # funciona
+
+
+
+
+print("\n\n\n datos de una tienda \n\n\n")
+print(get_data_by_id1(id="42305", entity_type="botiga"))
+
+print("\n\n\n todos los ids de tiendas \n\n\n")
+print(get_id_by_entityType1(entity_type="botiga"))
